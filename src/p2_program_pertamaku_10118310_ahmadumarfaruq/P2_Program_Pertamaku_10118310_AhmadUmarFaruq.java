83c3b12ff165c9f4d61153fdbf9e1a43605a3329
ahmadfaruq299 <61138778+ahmadfaruq299@users.noreply.github.com>
2020-03-31 21:18:26 +0700

    Update P2_Program_Pertamaku_10118310_AhmadUmarFaruq.java

---
+++ src/p2_program_pertamaku_10118310_ahmadumarfaruq/P2_Program_Pertamaku_10118310_AhmadUmarFaruq.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2_program_pertamaku_10118310_ahmadumarfaruq;
+
+/**
+ *
+ * @author AhmadFaruq
+ */
+public class P2_Program_Pertamaku_10118310_AhmadUmarFaruq {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            NewJFrame ut = new NewJFrame();
+            ut.setVisible(true);
+    }
+    
+}
